ce21a237391d02eaf7cdf816fecea783be7a0917
ashishkeshri514 <ashish.keshri.514@gmail.com>
2021-08-29 18:02:00 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import { AgGridReact} from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import data from './components/utils/country.json';
+import user from './components/utils/db.json';
+
 function App() {
   const [gridApi, setGridApi] =  useState(null);
-  const [tableData, setTableData]  = useState(null);
+  const [tableData, setTableData]  = useState(user.users);
   const [record, setRecord] = useState( {"id":"","name": "","email":"","gender":"","dob":"","city":"","country":""});
+  const [addtableData, setAddTableData]  = useState(null);
 
-  const url = 'http://localhost:4000/users';
+
+  // const url = 'http://localhost:4000/users';
   const columnDefs = [
     {headerName:"Id",field:"id",checkboxSelection: true},
     {headerName:"Name",field:"name"},
     {headerName:"Email",field:"email"},
     {headerName:"Gender",field:"gender"},
-    {headerName:"DOB",field:"dob",cellRenderer: date },
-    {headerName:"Country",field:"country",cellRenderer: country},
+    {headerName:"DOB",field:"dob" },
+    {headerName:"Country",field:"country"},
     {headerName:"City",field:"city"},
     {
       headerName: "",
@@ -25,6 +29,17 @@ function App() {
       editable: false,
       colId: "action"
     }
+
+  ]
+
+  const columnDefs2 = [
+    {headerName:"Id",field:"id",},
+    {headerName:"Name",field:"name"},
+    {headerName:"Email",field:"email"},
+    {headerName:"Gender",field:"gender"},
+    {headerName:"DOB",field:"dob"},
+    {headerName:"Country",field:"country"},
+    {headerName:"City",field:"city"},
 
   ]
   const onGridReady = (params) => {
@@ -36,19 +51,24 @@ function App() {
     sortable:true,
     flex:1,
     filter: false,
-    editable: true,
-    
+    editable: true, 
+  }
+  const defaultColDef2 = {
+    sortable:true,
+    flex:1,
+    filter: false,
+
   }
 
   function actionCellRenderer(params) {
     let eGui = document.createElement("div");
     eGui.innerHTML = `
-    <button class="action-button delete" data-action="delete" >  </button>
+    <button class="action-button delete" data-action="delete" > Delete </button>
     `;
     return eGui;
   }
 
-  fuction date() {
+  function date() {
     let eGui = document.createElement("div");
     eGui.innerHTML = `
     <input type="date" >  </button>
@@ -56,27 +76,52 @@ function App() {
     return eGui;
   }
 
-  fuction country() {
+  function gender() {
+    let data = {"Male":"male","Female":"female"};
     let dropdown = document.createElement("select");
 
     dropdown.length = 0;
+    dropdown.classList.add("custom-select");
 
     let defaultOption = document.createElement('option');
-    defaultOption.text = "Select Country";
+    defaultOption.text = "Select Gender";
     dropdown.add(defaultOption);
     dropdown.selectedIndex = 0;
     let option;
-    for(let i = 0; i< data.length; i++) {
+    
+    for (const [key, value] of Object.entries(data)) {
       option = document.createElement('option');
-      option.text = data[i];
-      option.value =data[i];
+      option.text = key;
+      option.value = key;
       dropdown.add(option);
     }
     
     return dropdown;
   }
 
+  function country() {
+    console.log(data);
+    let dropdown = document.createElement("select");
 
+    dropdown.length = 0;
+    dropdown.classList.add("custom-select");
+
+    let defaultOption = document.createElement('option');
+    defaultOption.text = "Select Country";
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+    let option;
+    
+    for (const [key, value] of Object.entries(data)) {
+      option = document.createElement('option');
+      option.text = key;
+      option.value = key;
+      dropdown.add(option);
+    }
+    
+    return dropdown;
+  }
+  
 
   function onCellClicked(params) {
     // Handle click event for action cells
@@ -89,50 +134,72 @@ function App() {
         });
       }
     }
+    return true;
   }
 
   const onAdd = () => {
-    setTableData([...tableData,record])
+    try{
+      setRecord({"id":"","name": "","email":"","gender":"","dob":"","city":"","country":""});
+      setAddTableData([...addtableData,record])
+      }
+    catch(err){
+      setRecord({"id":"","name": "","email":"","gender":"","dob":"","city":"","country":""});
+      setAddTableData([record]);
+      }
   }
 
   const onSelectedDelete = () =>{
-    const selectedNodes = gridApi.getSelectedNodes();
-    const selectedData = selectedNodes.map(node => node.data);
-    console.log(selectedData);
-    //To Implement
+    const selectedRows = gridApi.getSelectedRows();
+    setAddTableData(gridApi.applyTransaction({ remove: selectedRows }));
+    return true;
+    
   }
 
   const onNonSelectedDelete = () =>{
     const selectedNodes = gridApi.getSelectedNodes();
-    const selectedData = selectedNodes.map(node => node.data);
-    console.log(selectedData);
+    const nonselectedData = addtableData.filter(node => !(selectedNodes.includes(node)));
+    console.log(nonselectedData);
     //To Implement
   }
 
-  
-
-  
-
-  useEffect(()=>{
-    getUsers();
-  },[])
-
-  const getUsers = () =>{
-    fetch(url).then(res => res.json()).then(res=>setTableData(res))
+  const onSubmit = () => {  
+    setTableData([...tableData,...addtableData]);
+    setAddTableData(null);
   }
+
+  // useEffect(()=>{
+  //   getUsers();
+  // },[])
+  // const getUsers = () =>{
+ 
+  //   fetch(url).then(res => res.json()).then(res=>setTableData(res))
+   
+  // }
 
 
 return (
+  <div className="App">
     <div className="ag-theme-alpine" style={{height: 400,margin:8}}>
+      
       <div style={{display:'flex',justifyContent: 'space-evenly'}} className="crud-row">
         <button onClick={onAdd}>Add Row</button>
         <button onClick={onSelectedDelete}>Delete Selected Rows</button>
         <button onClick={onNonSelectedDelete}>Delete Non Selected Rows</button>
-        <button>Submit</button>
+        <button onClick={onSubmit}>Submit</button>
       </div>
+       <AgGridReact 
+        rowData={addtableData} 
+           columnDefs={columnDefs} defaultColDef={defaultColDef} onGridReady={onGridReady}  rowSelection="multiple"/>
+      <h1>Submitted Data</h1>
       <AgGridReact 
-          rowData={tableData} columnDefs={columnDefs} defaultColDef={defaultColDef} onGridReady={onGridReady}  rowSelection="multiple"/>
+          rowData={tableData} columnDefs={columnDefs2} defaultColDef={defaultColDef2}/>
+     
+    
+      
+     
     </div>
+
+  </div>
 );
 }
 
